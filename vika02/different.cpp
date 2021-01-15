cfc59cf7b20a8bf484bf2c9fbcfd6967a827b828
Bergur Snorrason <bsnorrason@gmail.com>
2021-01-15 11:54:18 +0000

    fdsa

---
+++ vika02/different.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long a, b;
+	while (cin >> a >> b)
+	{
+		cout << abs(a - b) << endl;
+	}
+}
---
+++ vika02/differentll.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+	ll a, b;
+	while (cin >> a >> b)
+	{
+		cout << abs(a - b) << endl;
+	}
+}
