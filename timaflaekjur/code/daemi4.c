2aae8b6f4de29bc96349f3488da4b11a37069b1d
Bergur Snorrason <bsnorrason@gmail.com>
2022-01-19 13:09:50 +0000

    -

--- timaflaekjur/code/daemi4.c
+++ timaflaekjur/code/daemi4.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
+int len(int n, int k)
+{
+	int r = 0;
+	while (n > 0) r++, n /= k;
+	return r;
+}
+
 int main()
 {
 	int i, j, n, r = 0;
 	scanf("%d", &n);
-	for (i = 0; i < n; i++)
-	for (j = 0; j < i/2; j++)
-		r += i^j;
+	for (i = 2; i <= n; i++) r += len(n, i);
 	printf("%d\n", r);
 	return 0;
 }
