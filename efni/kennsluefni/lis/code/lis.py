a4991f2fa4e0e052a8c32427c169f1628d94c172
Bergur Snorrason <bsnorrason@gmail.com>
2024-03-21 10:03:09 +0000

    .

--- efni/kennsluefni/lis/code/lis.py
+++ efni/kennsluefni/lis/code/lis.py
@@ -1,22 +1,23 @@
 from bisect import bisect
 
-def lis(a):
+def lis(a, weakly_increasing = True, return_indices = True):
     INF = 2**32
-    d = [INF for _ in range(len(a) + 1)]
-    e = d.copy()
+    s = 0 if weakly_increasing else -1
+    e, d = [INF]*(len(a) + 1), [INF]*(len(a) + 1)
     d[0] = -INF
     for i in range(len(a)):
-        r = bisect(d, a[i], 0, len(a) + 1)
+        r = bisect(d, a[i] + s, 0, len(d))
         d[r], e[i] = a[i], d[r - 1]
-
-    x = len(a)
-    while d[x] == INF: x -= 1
-    i, j, y, b = len(a) - 1, x - 1, d[x], [0 for _ in range(x)]
-    while j >= 0:
-        if a[i] == y: y, b[j] = e[i], a[i]; j -= 1
+    while d[-1] == INF: d.pop()
+    i, j, y = len(a) - 1, len(d) - 1, d[-1]
+    while j > 0:
+        if a[i] == y: y, d[j] = e[i], i; j -= 1
         i -= 1
-    return b;
+    return d[1:] if return_indices else list(map(lambda x: a[x], d[1:]))
 
-l = lis(list(map(int, input().split())))
-print(len(l))
-print(l)
+
+print(l := list(map(int, input().split())))
+print(lis(l, False, False))
+print(lis(l, False, True))
+print(lis(l, True, False))
+print(lis(l, True, True))
